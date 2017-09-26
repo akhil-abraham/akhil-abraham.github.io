0542c629dfff3cf20b6cebbc6b1a8c42d73c3a16
akhil-abraham <jessyjosemon@gmail.com>
2017-09-26 16:09:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  $("#test").hover(function(){
+    $("#test").removeClass("invisible");
+    $("#test").addClass("visible");
+  }, 
+  function(){
+    $("#test").removeClass("visible");
+    $("#test").addClass("invisible");
+  });
+});
